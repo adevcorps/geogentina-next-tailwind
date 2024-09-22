ddab4468dbe058f92c8d1747c1e4457b4c69447f
adevcorps <albert.devguy@gmail.com>
2024-09-22 15:59:37 +0200

    s5

--- components/PopupComponent/PopupComponent.tsx
+++ components/PopupComponent/PopupComponent.tsx
@@ -1,22 +1,31 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { PopupButton } from "react-calendly";
 
 const PopupComponent = ({ }) => {
   const [rootElement, setRootElement] = useState<HTMLElement | null>(null);
+  const [uploaded, setUploaded] = useState<Boolean>(false);
   useEffect(() => {
     if (typeof window !== "undefined") {
       setRootElement(document.getElementById("__next")!);
+      setUploaded(true);
     }
   }, []);
-  return (
 
-    <PopupButton
-        url="https://calendly.com/w-mkl-corp/geogentia-discovery-call"
-        rootElement={rootElement}
-        text="Schedule a demo"
-        className='inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-geo-gree text-secondary-foreground shadow-sm hover:bg-geo-gree/80 h-10 px-6 py-3'
-    />
-  );
+  if(rootElement !== null) {
+    return(<PopupButton
+      url="https://calendly.com/w-mkl-corp/geogentia-discovery-call"
+      rootElement={rootElement}
+      text="Schedule a demo"
+      className='inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-geo-gree text-secondary-foreground shadow-sm hover:bg-geo-gree/80 h-10 px-6 py-3'
+    />)
+  }
+  else{
+    return(
+      <></>
+    )
+  }
+    
+  // );
 };
 
 export default PopupComponent;
